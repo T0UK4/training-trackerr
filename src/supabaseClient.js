e0320d56565f73ad123d9c8b51dc9bd58c823d60
T0UK4 <lucas.lgrs@outlook.com>
2024-11-09 18:17:21 -0300

    Adiciona logs para debug

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://kvocbwkzlwfnyyxxbmmy.supabase.co';  // URL do seu projeto
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2b2Nid2t6bHdmbnl5eHhibW15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzExODQ3NDQsImV4cCI6MjA0Njc2MDc0NH0.swp5AntenGjFwawtC-xU1dQb2pqQiEJFLmSrYIjSybI';      // Chave anon/public
+// Substitua estas URLs pelas suas (não deixe as aspas vazias)
+const supabaseUrl = 'https://kvocbwkzlwfnyyxxbmmy.supabase.co';     // Cole sua Project URL aqui
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2b2Nid2t6bHdmbnl5eHhibW15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzExODQ3NDQsImV4cCI6MjA0Njc2MDc0NH0.swp5AntenGjFwawtC-xU1dQb2pqQiEJFLmSrYIjSybI';                   // Cole sua anon/public key aqui
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
